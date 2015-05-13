efb2573dca6a93cf8e9e358673bc4845f4deea58
0b623b013ea2321a820b9b80a06176daebf124a2 <frejraahede@gmail.com>
2015-05-13 18:59:56 +0200

    Adding js files

---
+++ tables/javascripts.js
@@ -0,0 +1,10 @@
+
+var tables = document.getElementsByClassName('compare');
+
+function setupCompareTable (el) {
+  console.log(el);
+}
+
+for (i = 0; i < tables.length; i++) {
+  setupCompareTable(tables[i]);
+}
