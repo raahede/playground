c5d2b5ec4c556ec247c8d74968384ebecaeb2b6a
0b623b013ea2321a820b9b80a06176daebf124a2 <frejraahede@gmail.com>
2015-05-16 13:13:14 +0200

    Adding sticky nav

---
+++ sticky-nav/javascripts.js
@@ -0,0 +1,51 @@
+
+var StickyNav = (function($) {
+  'use strict';
+
+  var $nav,
+      $clone,
+      headroomObj,
+      timeout;
+
+  function initialize() {
+    $nav = $('#sticky-nav');
+
+    if($nav.length) {
+      var promise = $.get('//cdnjs.cloudflare.com/ajax/libs/headroom/0.7.0/headroom.min.js');
+      promise.done(_setupSticky);
+    }
+  }
+
+  function _setupSticky() {
+    $clone = $nav.clone().addClass('sticky-clone');
+    $('body').append($clone);
+    headroomObj = new Headroom($clone[0], {
+      offset : _getNavOffset(),
+    }).init();
+    $(window).on('redraw', $.proxy(_onWindowResize, this));
+    $(window).on('resize', $.proxy(_onWindowResize, this));
+  }
+
+  function _getNavOffset() {
+    var offset = $nav.offset().top;
+    var height = $nav.outerHeight();
+    return offset + height;
+  }
+
+  function _onWindowResize( event ) {
+    // Debounce
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      headroomObj.offset = _getNavOffset();
+    }, 150);
+  }
+
+  return {
+    initialize: initialize
+  };
+
+})(jQuery);
+
+$(document).ready(function() {
+  StickyNav.initialize();
+});
